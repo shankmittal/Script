b4a4b383c4162520bac810ac19ffc1ab5033ff4d
Shashank Mittal <mittals@codeaurora.org>
2016-07-04 15:19:18 -0700

    Add setup script
    Change-Id: I061c7c8f3b262af560e8ed4da7ea994bebec6bb3

---
+++ setup.sh
@@ -0,0 +1,12 @@
+SCRIPT_PATH="`realpath \"$0\" | xargs dirname`"
+BACKUP=$HOME/scripts-backup/$(date -d "today" +"%Y-%m-%d_%H:%M")
+FILES=`ls -1 "$SCRIPT_PATH"`
+for f in $FILES; do
+	LINK=`echo $f | sed 's/^dot-/./'`;
+	if [ -e "$HOME/$LINK" ]
+	then
+		mkdir -p $BACKUP
+		mv $HOME/$LINK $BACKUP/$f
+	fi
+	ln -s $SCRIPT_PATH/$f $HOME/$LINK
+done
