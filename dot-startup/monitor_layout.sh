e33a689b897e95fa9433efd09d7e8e319e8b5de8
Shashank Mittal <mittals@codeaurora.org>
2016-07-04 14:46:27 -0700

    few more scripts
    Change-Id: I30719bfac01f810c92d0dd6ac0e607684daed3d6

---
+++ dot-startup/monitor_layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-5 --off --output DP-4 --off --output DP-3 --mode 1920x1080 --pos 1920x0 --rotate left --output DP-2 --mode 1920x1080 --pos 0x840 --rotate normal --output DP-1 --off --output DP-0 --off
---
+++ dot-startup/startup.sh
@@ -0,0 +1,10 @@
+# This is a startup script run by i3 window manager
+
+# Set monitors
+~/.startup/monitor_layout.sh
+
+# Set background
+xsetroot -solid "#333333"
+
+# Set synergyc
+synergyc mittals-lab.na.qualcomm.com
