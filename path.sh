adc094be16c2f36f7d9b65f5d5eda9f6844df978
Shashank Mittal <shankmittal@fb.com>
2019-04-29 23:57:50 -0700

    MAC scripts

---
+++ path.sh
@@ -0,0 +1,9 @@
+ANDROID_SDK=$HOME/opt/android-sdk
+
+PATH=$HOME/bin:$ANDROID_SDK/platform-tools:$ANDROID_SDK/tools:$PATH
+
+PATH=/opt/android_sdk/build-tools:$PATH
+
+# homebrew
+PATH=/Users/shankmittal/homebrew/bin:/Users/shankmittal/homebrew/opt/coreutils/libexec/gnubin:$PATH
+MANPATH=/Users/shankmittal/homebrew/opt/coreutils/libexec/gnuman:$MANPATH
