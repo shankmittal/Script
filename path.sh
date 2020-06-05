92b387825c023cac9a85913fc7a6fd3676a9bfcb
Shashank Mittal <shankmittal@gmail.com>
2020-06-04 23:25:27 -0700

    mac-changes

--- path.sh
+++ path.sh
@@ -1,9 +1,11 @@
 ANDROID_SDK=$HOME/opt/android-sdk
 
-PATH=$HOME/bin:$ANDROID_SDK/platform-tools:$ANDROID_SDK/tools:$PATH
+PATH=$HOME/bin:$HOME/opt/bin:$ANDROID_SDK/platform-tools:$ANDROID_SDK/tools:$PATH
 
 PATH=/opt/android_sdk/build-tools:$PATH
 
 # homebrew
-PATH=/Users/shankmittal/homebrew/bin:/Users/shankmittal/homebrew/opt/coreutils/libexec/gnubin:$PATH
-MANPATH=/Users/shankmittal/homebrew/opt/coreutils/libexec/gnuman:$MANPATH
+#PATH=/Users/shankmittal/homebrew/bin:/Users/shankmittal/homebrew/opt/coreutils/libexec/gnubin:$PATH
+#MANPATH=/Users/shankmittal/homebrew/opt/coreutils/libexec/gnuman:$MANPATH
+
+PATH=/opt/homebrew/bin:/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH
